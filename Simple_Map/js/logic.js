38c875f5efb43d327c5bd4abe921e954e7570794
Valeria Rodriguez <bigv@Valerias-MBP.lan>
2022-06-30 22:22:07 -0400

    d

--- Simple_Map/js/logic.js
+++ Simple_Map/js/logic.js
@@ -1,20 +1,14 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
+// We create the map object with options.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '© OpenStreetMap'
-}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    accessToken: //styles/mapbox/dark-v10
+    accessToken: 'pk.eyJ1Ijoic2tpbm55dHdpbnZhbGUiLCJhIjoiY2w0eGR2eGlyMWJwNDNjbW5kZDVpbm93aSJ9.ZlQLaulrg3syWRCZf7idcQ'
 });
-
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'streets' tile layer to the map.
 streets.addTo(map);
